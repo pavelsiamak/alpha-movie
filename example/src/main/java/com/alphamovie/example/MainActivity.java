48a988551ee7558df06f9cf5334438194253e119
pavel.semak <pavel.semak>
2017-03-10 00:02:51 +0300

    Image resources for different screens

--- example/src/main/java/com/alphamovie/example/MainActivity.java
+++ example/src/main/java/com/alphamovie/example/MainActivity.java
@@ -9,14 +9,21 @@ import android.view.View;
 import com.alphamovie.lib.AlphaMovieView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int[] backgroundResources = new int[]{R.drawable.court_blue,
+            R.drawable.court_green, R.drawable.court_red};
 
     private AlphaMovieView alphaMovieView;
+
+    private View container;
+    private int bgIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFormat(PixelFormat.TRANSLUCENT);
         setContentView(R.layout.activity_main);
+
+        container = findViewById(R.id.container);
 
         alphaMovieView = (AlphaMovieView) findViewById(R.id.video_player);
         alphaMovieView.setVideoFromAssets("ball.mp4");
@@ -45,6 +52,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void stop(View view) {
         alphaMovieView.stop();
+    }
+
+    public void changeBackground(View view) {
+        bgIndex = ++bgIndex % 3;
+        container.setBackgroundResource(backgroundResources[bgIndex]);
     }
 
     public void addNew(View view) {
